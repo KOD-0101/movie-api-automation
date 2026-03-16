f8d63ecd3bd4694754a64b46fafbb9278ac4a194
KOD-0101 <dikekerichi@gmail.com>
2026-03-16 20:18:52 +0000

    Add Streamlit dashboard

--- get_top_movies.py
+++ get_top_movies.py
@@ -1,38 +1,65 @@
 import requests
 import os
-import csv
+import sqlite3
+from datetime import datetime
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 API_KEY = os.getenv("TMDB_API_KEY")
 
-url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}"
+if not API_KEY:
+    print("Error: API key not found.")
+    exit()
 
-response = requests.get(url)
-data = response.json()
+today = datetime.today().strftime("%Y-%m-%d")
 
-print("\nTop Rated Movies:\n")
+# Connect to database (creates file if it doesn't exist)
+conn = sqlite3.connect("movies.db")
+cursor = conn.cursor()
 
-movies = data["results"][:5]
+# Create table if it doesn't exist
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS movies (
+    date TEXT,
+    title TEXT,
+    rating REAL,
+    votes INTEGER,
+    release_date TEXT,
+    popularity REAL
+)
+""")
 
-for movie in movies:
-    print(movie["title"], "⭐", movie["vote_average"])
+# Collect movies
+for page in range(1, 6):
 
+    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&page={page}"
 
-# Save to CSV
-with open("top_movies.csv", "w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
+    response = requests.get(url)
 
-    # header
-    writer.writerow(["Title", "Rating", "Release Date"])
+    if response.status_code != 200:
+        print("API request failed:", response.status_code)
+        exit()
 
-    # rows
-    for movie in movies:
-        writer.writerow([
-            movie["title"],
-            movie["vote_average"],
-            movie["release_date"]
-        ])
+    data = response.json()
 
-print("\nMovies saved to top_movies.csv")
+    for movie in data["results"]:
+
+        cursor.execute("""
+        INSERT INTO movies (date,title,rating,votes,release_date,popularity)
+        VALUES (?,?,?,?,?,?)
+        """, (
+            today,
+            movie.get("title"),
+            movie.get("vote_average"),
+            movie.get("vote_count"),
+            movie.get("release_date"),
+            movie.get("popularity")
+        ))
+
+# Save changes
+conn.commit()
+conn.close()
+
+print("Movie database updated successfully.")
